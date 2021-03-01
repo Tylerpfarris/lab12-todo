b4d876d065836c85bfd7b73a7a6e29762869c905
Tyler Farris <Tylerpfarris@gmail.com>
2021-03-01 13:13:15 -0800

    data schema updated with todos

---
+++ data/to-dos.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    todo: 'wash the dishes',
+    completed: false,
+    user_id: 4
+  },
+  {
+    todo: 'do laundry',
+    completed: false,
+    user_id: 4
+  },
+  {
+    todo: 'code',
+    completed: false,
+    user_id: 4
+  }
+];
